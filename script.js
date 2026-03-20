f532c30e6923b438559fd4d5b58367faf5af44df
manuellla lopes <manuellaggomeslopes@gmail.com>
2026-03-20 11:24:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function scrollToTrailer() {
     document.getElementById("trailer").scrollIntoView({ behavior: "smooth" });
 }
@@ -9,17 +8,9 @@ window.addEventListener("scroll", () => {
     fadeElements.forEach(el => {
         const position = el.getBoundingClientRect().top;
         const screenPosition = window.innerHeight / 1.2;
+
         if (position < screenPosition) {
             el.classList.add("show");
         }
     });
 });
-=======
-<script>
-function scrollToTrailer() {
-    document.getElementById("trailer").scrollIntoView({
-        behavior: "smooth"
-    })
-}
-</script>
->>>>>>> 054ba77c9e65bee8120dc8784081f1a85e588ef6
